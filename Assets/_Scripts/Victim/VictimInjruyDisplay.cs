0f560133ad356a363b212e327fa09cabc216c24c
unknown <b_m174@txstate.edu>
2018-06-29 20:56:13 -0500

    Added VictimInjruyDisplay script that will be used to manipulate the intensity of the masks on the Victims (How much blood is going to present on the material). Currently only has the variables needed and their initialization.

---
+++ Assets/_Scripts/Victim/VictimInjruyDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimInjruyDisplay : MonoBehaviour {
+    GameObject body;
+    Victim victim;
+    Renderer renderer;
+    Material headMaterial;
+    Material bodyMaterial;
+    Texture headInjuryMask;
+    Texture bodyInjuryMask;
+    public List<GameObject> headInjuryMasks= new List<GameObject>();
+    public List<GameObject> bodyInjuryMasks= new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = transform.Find("CC_Base_Body").gameObject;
+        renderer = body.GetComponent<Renderer>();
+        bodyMaterial = renderer.materials[0];
+        headMaterial = renderer.materials[1];
+        bodyInjuryMask = bodyMaterial.GetTexture("InjuryMask");
+        headInjuryMask = headMaterial.GetTexture("InjuryMask");
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
