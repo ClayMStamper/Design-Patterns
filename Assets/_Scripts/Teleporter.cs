822507840222a567464d4197d7030cc4f3b63a97
p_c143 <p_c143@txstate.edu>
2018-08-11 16:09:59 -0500

    bandage warping fixed and fadein added to ambus teleport

--- Assets/_Scripts/Teleporter.cs
+++ Assets/_Scripts/Teleporter.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    public Vector3 offsetFromCollider;
+    //pos to teleport to
+    [SerializeField]
+    private Transform marker;
+    [SerializeField]
+    OVRScreenFade fade;
 
     public GameObject teleportLocation;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(marker.position, 0.25f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
-           other.transform.SetPositionAndRotation(teleportLocation.transform.position + offsetFromCollider, Quaternion.LookRotation(teleportLocation.transform.forward));
+        if (other.CompareTag("Player"))
+            StartCoroutine(FadeAndTeleport(other.transform));
+    }
+
+
+    IEnumerator FadeAndTeleport(Transform player)
+    {
+
+        Debug.Log("Starting fade");
+
+        IEnumerator e = fade.Fade(0, 1);
+        while (e.MoveNext())
+            yield return e.Current;
+
+        player.position = marker.position;
+
+        e = fade.Fade(1, 0);
+        while (e.MoveNext())
+            yield return e.Current;
+
+        Debug.Log("Finished fade");
+
+        yield return null;
+
     }
 
 }
