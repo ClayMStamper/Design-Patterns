b678fbb08d3074b1c2df2be494f775d2b86da8a7
p_c143 <p_c143@txstate.edu>
2018-08-13 11:01:43 -0500

    Extra comments and minor organization changes

--- Assets/_Scripts/Teleporter.cs
+++ Assets/_Scripts/Teleporter.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Function: Teleports the player in and out of the bus
+ * Instances: Attached to "Teleporter" objects
+ */
+
 public class Teleporter : MonoBehaviour
 {
-    //pos to teleport to
+    //pos to teleport to: attached to child
     [SerializeField]
     private Transform marker;
+
+    //Fade script: attached to OVRCameraRig
     [SerializeField]
     OVRScreenFade fade;
 
     public GameObject teleportLocation;
 
+    //draw light blue wire sphere on teleport target in the editor
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
@@ -30,19 +38,20 @@ public class Teleporter : MonoBehaviour
 
         Debug.Log("Starting fade");
 
+        //start fade out
         IEnumerator e = fade.Fade(0, 1);
-        while (e.MoveNext())
+        while (e.MoveNext()) //wait for fade out
             yield return e.Current;
 
+        //teleport
         player.position = marker.position;
 
+        //start fade in
         e = fade.Fade(1, 0);
-        while (e.MoveNext())
+        while (e.MoveNext()) //wait
             yield return e.Current;
 
         Debug.Log("Finished fade");
-
-        yield return null;
 
     }
 
