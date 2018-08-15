cecdf3212db53a79cdcf2bb7dd627ef8bc857996
p_c143 <p_c143@txstate.edu>
2018-08-15 14:48:00 -0500

    Progress update for scene prefab workflow solution. No assets changed

---
+++ Assets/_Scripts/Util/ScenePrefab.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum SceneCategory {Ambus, MCI}
+
+public class ScenePrefab : MonoBehaviour {
+
+    [SerializeField]
+    private new string name;
+
+    [SerializeField]
+    private SceneCategory sceneCategory;
+
+    [ContextMenu ("Set Prefab")]
+    public void SetPrefab()
+    {
+
+        //duplicate gameobject
+
+        //prefab duplicated gameobject
+        try
+        {
+            switch (sceneCategory)
+            {
+                case SceneCategory.Ambus:
+                    PrefabUtility.CreatePrefab("Assets/_Prefabs/Ambus/ScenePrefabs", gameObject);
+                    break;
+                case SceneCategory.MCI:
+                    PrefabUtility.CreatePrefab("Assets/_Prefabs/MassCasualty/ScenePrefabs", gameObject);
+                    break;
+                default:
+                    Debug.LogError("Scene category not recognized");
+                    break;
+            }
+        } catch
+        {
+            Debug.LogError("Failed to create new prefab");
+        }
+
+        //destroy gameObject
+
+    }
+
+
+}
