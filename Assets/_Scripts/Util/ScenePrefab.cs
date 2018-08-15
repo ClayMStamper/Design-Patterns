4ac3049c8af84ba96858192f308289c0baf19e96
p_c143 <p_c143@txstate.edu>
2018-08-15 15:13:56 -0500

    Prefab worklflow works all but prefabbed objects still can't have prefabbed childre. Development gameobjects need to be detached from their high level prefabs if your feature requires low level prefabs

---
+++ Assets/_Scripts/Util/ScenePrefab.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum SceneCategory {Ambus, MCI}
+
+public class ScenePrefab : MonoBehaviour {
+
+    [SerializeField]
+    private new string name;
+
+    [SerializeField]
+    private SceneCategory sceneCategory;
+
+    [ContextMenu ("Set Prefab")]
+    public void SetPrefab()
+    {
+
+        //duplicate gameobject
+
+        //prefab duplicated gameobject
+        try
+        {
+
+            if (name == "")
+            {
+                Debug.LogError("Can't commit nameless prefab");
+                return;
+            }
+
+            switch (sceneCategory)
+            {
+                case SceneCategory.Ambus:
+                    PrefabUtility.CreatePrefab(("Assets/_Prefabs/Ambus/ScenePrefabs/" + name + ".prefab"), gameObject);
+                    break;
+                case SceneCategory.MCI:
+                    PrefabUtility.CreatePrefab(("Assets/_Prefabs/MassCasualty/ScenePrefabs" + name + ".prefab"), gameObject);
+                    break;
+                default:
+                    Debug.LogError("Scene category not recognized");
+                    break;
+            }
+        } catch
+        {
+            Debug.LogError("Failed to create new prefab");
+        }
+
+    }
+
+
+}
