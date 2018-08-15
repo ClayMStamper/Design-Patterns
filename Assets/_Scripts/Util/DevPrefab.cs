ac88b6884443cb56e57dce8ecba4e5dc7649e2a6
p_c143 <p_c143@txstate.edu>
2018-08-15 15:34:08 -0500

    DevPrefab.cs added: relinks broken prefabs

---
+++ Assets/_Scripts/Util/DevPrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class DevPrefab : MonoBehaviour {
+
+    [SerializeField]
+    GameObject prefabLink;
+
+    [ContextMenu ("Link Prefab")]
+	void LinkPrefab()
+    {
+   
+        try
+        {
+            PrefabUtility.ConnectGameObjectToPrefab(gameObject, prefabLink);
+        } catch 
+        {
+            Debug.LogError("Error linking Gameobject " + name + " to prefab " + prefabLink); //+ ":" + e.Message);
+        }
+    
+    }
+}
