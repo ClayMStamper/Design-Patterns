207ba54a3c460389a6ab481e06ad3cf7c19c2e60
p_c143 <p_c143@txstate.edu>
2018-08-15 15:13:56 -0500

    Prefab worklflow works all but prefabbed objects still can't have prefabbed childre. Development gameobjects need to be detached from their high level prefabs if your feature requires low level prefabs
    DevPrefab.cs added: relinks broken prefabs
    
    New working prefab workflow. Only issue is relinking prefabs also resets position
    
    Encapsulated functionality to highest level prefab
    
    mend
    
    Prefab workflow finished, tested, and commented

---
+++ Assets/_Scripts/Util/LowLvlPrefab.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowLvlPrefab : MonoBehaviour {
+
+    public GameObject prefabLink;
+
+}
---
+++ Assets/_Scripts/Util/ScenePrefab.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+/*
+ * Author: Clayton
+ * 
+ * Use Case: When adding functionality unlink prefab by right clicking this component and select the "Relink" function.
+ * 
+ * Requirements: 
+ * - Reference low lvl prefabs in serialized list
+ * - Low lvl prefabs have LowLvlPrefab.cs attached
+ * - Reference prefab link in each low lvl prefab
+ * 
+ */
+public class ScenePrefab : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> LowLvlPrefabs;
+
+    [ContextMenu("Relink Low Lvl Prefabs")]
+    void LinkPrefabs()
+    {
+
+        //disconnect high level prefab
+        PrefabUtility.DisconnectPrefabInstance(gameObject);
+
+        //cycly through and relink low lvl prfabs
+        foreach (GameObject obj in LowLvlPrefabs)
+        {
+            
+            //cache references for when obj is destroyed
+            GameObject newObj = null;
+            Vector3 pos = obj.transform.position;
+
+            GameObject prefabLink = obj.GetComponent<LowLvlPrefab>().prefabLink;
+
+            try
+            {
+                //relink prefab (destroys current obj)
+                newObj = PrefabUtility.ConnectGameObjectToPrefab(obj, prefabLink);
+            }
+            catch
+            {
+                Debug.LogError("Error linking Gameobject " + name + " to prefab " + prefabLink); //+ ":" + e.Message);
+            }
+
+            //reset to correct position
+            newObj.transform.position = pos;
+
+        }
+
+    }
+
+}
