92de9956123652199649db353feabbb95fc28a2f
p_c143 <p_c143@txstate.edu>
2018-08-22 13:00:36 -0500

    Lerp script written for hologram, not functional yet

---
+++ Assets/_Scripts/WatchGuiLerp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchGuiLerp : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    [SerializeField] [Range(0, 1)]
+    public float smoothSpeed = 0.125f;
+    [SerializeField]
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        UpdatePos();
+    }
+
+    [ContextMenu("Set Position")]
+    void UpdatePos()
+    {
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+
+        transform.position = smoothedPosition;
+
+    }
+
+}
