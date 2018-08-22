968e85b3fe8902638c43f93da56f2b10a5f53e71
p_c143 <p_c143@txstate.edu>
2018-08-22 15:27:27 -0500

    Drawer functionality abstracted from the gameobject so that it's seperate from the model. Drawers are ready for implementing item float functionality

--- Assets/_Scripts/DrawerContent.cs
+++ Assets/_Scripts/DrawerContent.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class DrawerContent : MonoBehaviour {
 
-	
+    [SerializeField]
+    GameObject item;
+    [SerializeField] [Tooltip ("Where item spawns from")]
+    Transform origin;
+
+    [SerializeField] [Tooltip ("How high item floats out of drawer")]
+    float floatDistance = 0.2f;
+
+    [HideInInspector]
+    public GameObject currentItem;
+
+    //the current item
+    public void UnsetCurrentItem()
+    {
+
+        currentItem = null;
+        SpawnNewItem();
+
+    }
+
+    private void SpawnNewItem()
+    {
+
+    }
 }
---
+++ Assets/_Scripts/MedicalItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicalItem : MonoBehaviour, ISpecialGrabbable
+{
+    public void Interact()
+    {
+        if (transform.parent.GetComponent <DrawerContent>()) //is drawer
+        {
+
+            DrawerContent parent = transform.parent.GetComponent<DrawerContent>();
+            parent.UnsetCurrentItem();
+
+        }
+    }
+
+}
